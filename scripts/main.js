f32f677cc6c0b20bcf0d24f4918073a45d42c7eb
Cefas Sathler <cefasheli@gmail.com>
2023-03-02 10:36:16 -0300

    upload

---
+++ scripts/main.js
@@ -0,0 +1,66 @@
+  
+const moduleName = 'custom-dice-d6';
+
+Hooks.once('diceSoNiceReady', (dice3d) => {
+  // MASKS
+  dice3d.addSystem({id:"memento", name:"Memento"}, false);
+  dice3d.addDicePreset({
+    type:"d6",
+    system:"memento",
+    labels:[
+      'modules/' + moduleName + '/images/memento/d6-1.webp', 
+      'modules/' + moduleName + '/images/memento/d6-2.webp', 
+      'modules/' + moduleName + '/images/memento/d6-3.webp',
+      'modules/' + moduleName + '/images/memento/d6-4.webp', 
+      'modules/' + moduleName + '/images/memento/d6-5.webp', 		
+      'modules/' + moduleName + '/images/memento/d6-6.webp'
+    ],
+    bumpMaps:[
+      'modules/' + moduleName + '/images/memento/d6-1-b.webp', 
+      'modules/' + moduleName + '/images/memento/d6-2-b.webp', 
+      'modules/' + moduleName + '/images/memento/d6-3-b.webp',
+      'modules/' + moduleName + '/images/memento/d6-4-b.webp',		
+      'modules/' + moduleName + '/images/memento/d6-5-b.webp',
+      'modules/' + moduleName + '/images/memento/d6-6-b.webp'
+    ]      
+  });
+
+
+  
+    // AVATAR
+  dice3d.addSystem({id:"cyberpunk", name:"Cyberpunk"}, false);
+  dice3d.addDicePreset({
+    type:"d6",
+    system:"cyberpunk",
+    labels:[
+      'modules/' + moduleName + '/images/cyberpunk/a.png', 
+      'modules/' + moduleName + '/images/cyberpunk/b.png', 
+      'modules/' + moduleName + '/images/cyberpunk/c.png',
+      'modules/' + moduleName + '/images/cyberpunk/d.png', 
+      'modules/' + moduleName + '/images/cyberpunk/e.png', 		
+      'modules/' + moduleName + '/images/cyberpunk/f.png'
+    ],
+    bumpMaps:[
+      'modules/' + moduleName + '/images/cyberpunk/1.png', 
+      'modules/' + moduleName + '/images/cyberpunk/2.png', 
+      'modules/' + moduleName + '/images/cyberpunk/3.png',
+      'modules/' + moduleName + '/images/cyberpunk/4.png',		
+      'modules/' + moduleName + '/images/cyberpunk/5.png',
+      'modules/' + moduleName + '/images/cyberpunk/6.png'
+    ]      
+  });
+  // AVATAR
+  dice3d.addSystem({id:"memesssnto2", name:"sss"}, false);
+  dice3d.addDicePreset({
+    type:"dc",
+    system:"sssss",
+    labels:[
+      'modules/' + moduleName + '/images/memento/heads.webp', 
+      'modules/' + moduleName + '/images/memento/tail.webp'
+    ],
+    bumpMaps:[
+      'modules/' + moduleName + '/images/memento/heads_bump.webp', 
+      'modules/' + moduleName + '/images/memento/tail_bump.webp'
+    ]      
+  });
+});
